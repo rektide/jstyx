e2d43c4095273e20d49b9129090b4caf7fda901b
jonblower <jonblower@aeb1b72f-4910-0410-8ae4-b0e1c25358ee>
2005-03-02 08:13:34 +0000

    set command-line options correctly
    git-svn-id: svn://svn.code.sf.net/p/jstyx/code/trunk@79 aeb1b72f-4910-0410-8ae4-b0e1c25358ee

--- core/bin/JStyxRun.bat
+++ core/bin/JStyxRun.bat
@@ -4,6 +4,6 @@ set CP=..\conf
 for %%i in ("..\lib\*.jar") do call catenv.bat %%i
 for %%i in ("..\dist\*.jar") do call catenv.bat %%i
 
-set OPTS = -Djava.protocol.handler.pkgs=uk.ac.rdg.resc.jstyx.client.protocol
+set OPTS=-Djava.protocol.handler.pkgs=uk.ac.rdg.resc.jstyx.client.protocol
 
 java %OPTS% -cp %CP% %*
