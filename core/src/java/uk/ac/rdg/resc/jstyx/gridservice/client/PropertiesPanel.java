94f408875f6a6c0477785335962ff59a760d58e4
jonblower <jonblower@aeb1b72f-4910-0410-8ae4-b0e1c25358ee>
2005-05-17 15:50:43 +0000

    Initial import
    git-svn-id: svn://svn.code.sf.net/p/jstyx/code/trunk@243 aeb1b72f-4910-0410-8ae4-b0e1c25358ee

---
+++ core/src/java/uk/ac/rdg/resc/jstyx/gridservice/client/PropertiesPanel.java
@@ -0,0 +1,104 @@
+/*
+ * Copyright (c) 2005 The University of Reading
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the University of Reading, nor the names of the
+ *    authors or contributors may be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package uk.ac.rdg.resc.jstyx.gridservice.client;
+
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import java.awt.BorderLayout;
+
+import javax.swing.event.TreeSelectionListener;
+import javax.swing.event.TreeSelectionEvent;
+
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.TreePath;
+
+/**
+ * Panel for displaying properties of an SGS server, service or instance
+ *
+ * @author Jon Blower
+ * $Revision$
+ * $Date$
+ * $Log$
+ * Revision 1.1  2005/05/17 15:50:43  jonblower
+ * Initial import
+ *
+ */
+public class PropertiesPanel extends JPanel implements TreeSelectionListener
+{
+    
+    private JLabel label;
+    
+    /** Creates a new instance of PropertiesPanel */
+    public PropertiesPanel()
+    {
+        this.setLayout(new BorderLayout());
+        this.label = new JLabel("Properties");
+        this.add(this.label, BorderLayout.CENTER);
+    }
+    
+    /**
+     * This is called when nodes in the SGSExplorer's tree have been selected
+     * or deselected.
+     */
+    public void valueChanged(TreeSelectionEvent e) 
+    {
+        TreePath leadPath = e.getNewLeadSelectionPath();
+        if (leadPath != null)
+        {
+            DefaultMutableTreeNode node =
+                (DefaultMutableTreeNode)leadPath.getLastPathComponent();
+            String nodeType;
+            int depthInTree = node.getPath().length - 1;
+            if (depthInTree == 0)
+            {
+                // Shouldn't be selectable
+                nodeType = "root";
+            }
+            else if (depthInTree == 1)
+            {
+                nodeType = "server";
+            }
+            else if (depthInTree == 2)
+            {
+                nodeType = "service";
+            }
+            else if (depthInTree == 3)
+            {
+                nodeType = "instance";
+            }
+            else
+            {
+                // Shoudn't happen
+                nodeType = "unknown";
+            }
+            this.label.setText(nodeType + ": " + node.toString());
+            this.repaint();
+        }
+    }
+}
