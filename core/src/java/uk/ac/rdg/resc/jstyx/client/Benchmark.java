037cc1b5dd2b2a9d5d415a5b4835c254a0e7cc4a
jonblower <jonblower@aeb1b72f-4910-0410-8ae4-b0e1c25358ee>
2005-06-20 17:21:51 +0000

    Initial import
    git-svn-id: svn://svn.code.sf.net/p/jstyx/code/trunk@298 aeb1b72f-4910-0410-8ae4-b0e1c25358ee

---
+++ core/src/java/uk/ac/rdg/resc/jstyx/client/Benchmark.java
@@ -0,0 +1,80 @@
+/*
+ * Benchmark.java
+ *
+ * Created on 20 June 2005, 14:26
+ *
+ * To change this template, choose Tools | Options and locate the template under
+ * the Source Creation and Management node. Right-click the template and choose
+ * Open. You can then make changes to the template in the Source Editor.
+ */
+
+package uk.ac.rdg.resc.jstyx.client;
+
+import java.net.URL;
+import java.io.BufferedInputStream;
+import java.io.InputStream;
+
+import uk.ac.rdg.resc.jstyx.client.StyxConnection;
+import uk.ac.rdg.resc.jstyx.client.CStyxFile;
+import uk.ac.rdg.resc.jstyx.client.StyxFileInputStream;
+
+/**
+ * Program that compares the downloading of a file via HTTP and Styx, with
+ * various message sizes
+ * @author jdb
+ */
+public class Benchmark
+{
+    
+    public static void main (String[] args) throws Exception
+    {
+        String filename = "jdk-1_5_0_03-linux-i586.rpm";
+        String httpRoot = "http://www.resc.rdg.ac.uk/";
+        String styxRoot = "styx://www.resc.rdg.ac.uk:9876/";
+        //downloadFromURL(httpRoot + filename);
+        downloadFromStyxURL(styxRoot + filename);
+    }
+    
+    /**
+     * Downloads data via an InputStream
+     */
+    private static void downloadFromURL(String urlStr) throws Exception
+    {
+        System.out.println("Downloading from " + urlStr);
+        URL url = new URL(urlStr);
+        InputStream is = url.openStream();
+        long start = System.currentTimeMillis();
+        int n;
+        byte[] bytes = new byte[16000];
+        do
+        {
+            n = is.read(bytes);
+        } while (n >= 0);
+        long time = System.currentTimeMillis() - start;
+        System.out.println("Download took " + time + " milliseconds");
+        is.close();
+    }
+    
+    /**
+     * Downloads data via an InputStream
+     */
+    private static void downloadFromStyxURL(String urlStr) throws Exception
+    {
+        System.out.println("Downloading from " + urlStr);
+        URL url = new URL(urlStr);
+        StyxConnection conn = new StyxConnection(url.getHost(), url.getPort(), 65536);
+        conn.connect();
+        CStyxFile file = conn.getFile(url.getPath());
+        StyxFileInputStream is = new StyxFileInputStream(file);
+        long start = System.currentTimeMillis();
+        int n;
+        byte[] bytes = new byte[16000];
+        do
+        {
+            n = is.read(bytes);
+        } while (n >= 0);
+        long time = System.currentTimeMillis() - start;
+        System.out.println("Download took " + time + " milliseconds");
+        is.close();
+    }
+}
