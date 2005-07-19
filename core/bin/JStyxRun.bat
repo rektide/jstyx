5ab6b60b024fa489410515e68cc1c3841fce6e80
jonblower <jonblower@aeb1b72f-4910-0410-8ae4-b0e1c25358ee>
2005-07-19 08:03:47 +0000

    Changes to reflect new directory structure
    git-svn-id: svn://svn.code.sf.net/p/jstyx/code/trunk@319 aeb1b72f-4910-0410-8ae4-b0e1c25358ee

--- core/bin/JStyxRun.bat
+++ core/bin/JStyxRun.bat
@@ -2,7 +2,7 @@
 
 set CP=..\conf
 for %%i in ("..\lib\*.jar") do call catenv.bat %%i
-for %%i in ("..\dist\*.jar") do call catenv.bat %%i
+for %%i in ("..\target\*.jar") do call catenv.bat %%i
 
 set OPTS=-Djava.protocol.handler.pkgs=uk.ac.rdg.resc.jstyx.client.protocol
 
