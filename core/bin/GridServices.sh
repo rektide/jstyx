9b94a0a296053c0e77fe6afb146b230564f0cf1a
jonblower <jonblower@aeb1b72f-4910-0410-8ae4-b0e1c25358ee>
2005-08-03 09:34:32 +0000

    Corrected capitalisation in SGSconfig.xml
    git-svn-id: svn://svn.code.sf.net/p/jstyx/code/trunk@342 aeb1b72f-4910-0410-8ae4-b0e1c25358ee

--- core/bin/GridServices.sh
+++ core/bin/GridServices.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-./JStyxRun.sh uk.ac.rdg.resc.jstyx.gridservice.server.StyxGridService ../conf/SGSConfig.xml
+./JStyxRun.sh uk.ac.rdg.resc.jstyx.gridservice.server.StyxGridService ../conf/SGSconfig.xml
