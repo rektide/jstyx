bc89f780976e081d661910c9723be0c4ec63e2c5
jonblower <jonblower@aeb1b72f-4910-0410-8ae4-b0e1c25358ee>
2005-11-09 17:49:58 +0000

    Implemented File subclass (representing fixed-name input files)
    git-svn-id: svn://svn.code.sf.net/p/jstyx/code/trunk@480 aeb1b72f-4910-0410-8ae4-b0e1c25358ee

--- core/src/java/uk/ac/rdg/resc/jstyx/gridservice/server/SGSInputFile.java
+++ core/src/java/uk/ac/rdg/resc/jstyx/gridservice/server/SGSInputFile.java
@@ -35,8 +35,11 @@ import java.net.URL;
 import org.apache.mina.common.ByteBuffer;
 
 import uk.ac.rdg.resc.jstyx.StyxException;
+import uk.ac.rdg.resc.jstyx.types.ULong;
 import uk.ac.rdg.resc.jstyx.server.StyxFile;
 import uk.ac.rdg.resc.jstyx.server.StyxFileClient;
+import uk.ac.rdg.resc.jstyx.server.FileOnDisk;
+import uk.ac.rdg.resc.jstyx.server.URLFile;
 
 /**
  * A file that is used to provide input (as a file or as stdin stream) to a 
@@ -46,6 +49,9 @@ import uk.ac.rdg.resc.jstyx.server.StyxFileClient;
  * $Revision$
  * $Date$
  * $Log$
+ * Revision 1.3  2005/11/09 17:49:58  jonblower
+ * Implemented File subclass (representing fixed-name input files)
+ *
  * Revision 1.2  2005/11/04 19:29:53  jonblower
  * Moved code that writes to std input to SGSInputFile
  *
@@ -58,13 +64,16 @@ public abstract class SGSInputFile extends StyxFile
     
     protected InputURLFile urlFile;
     protected StyxGridServiceInstance instance;
+    protected boolean dataWritten;  // True when any data have been written to this file
+                                    // (don't necessarily have to have reached EOF)
     
-    private SGSInputFile(String name, InputURLFile urlFile, StyxGridServiceInstance instance)
+    private SGSInputFile(String name, StyxGridServiceInstance instance)
         throws StyxException
     {
         super(name, 0222); // Input files are always write-only
-        this.urlFile = urlFile;
         this.instance = instance;
+        this.urlFile = new InputURLFile(name);
+        this.dataWritten = false;
     }
     
     /**
@@ -78,10 +87,10 @@ public abstract class SGSInputFile extends StyxFile
         /**
          * Creates new StdinFile - will be called "stdin"
          */
-        public StdinFile(InputURLFile urlFile, StyxGridServiceInstance instance)
+        public StdinFile(StyxGridServiceInstance instance)
             throws StyxException
         {
-            super("stdin", urlFile, instance);
+            super("stdin", instance);
         }
 
         public void setOutputStream(OutputStream os)
@@ -108,12 +117,12 @@ public abstract class SGSInputFile extends StyxFile
                 throw new StyxException("Cannot write to the standard input" +
                     " when the service is not running");
             }
-            else if (this.getInputURL() != null)
+            else if (this.urlFile.getURL() != null)
             {
                 // We're not allowed to write to this file if the service is running
                 // and the input URL is set
                 throw new StyxException("Cannot write to the input stream " +
-                    "because the service is reading from " + this.getInputURL());
+                    "because the service is reading from " + this.urlFile.getURL());
             }
             else
             {
@@ -149,35 +158,123 @@ public abstract class SGSInputFile extends StyxFile
         }
     }
     
-    /**
-     * @return the url from which this file will get its data, or null if this
-     * has not been set
-     */
-    public URL getInputURL()
-    {
-        return this.urlFile.getURL();
-    }
-    
     public static class File extends SGSInputFile
     {
-        private File(String name, InputURLFile urlFile,
-            StyxGridServiceInstance instance)
+        private FileOnDisk fileOnDisk;
+        private java.io.File file;
+        
+        public File(java.io.File file, StyxGridServiceInstance instance)
             throws StyxException
         {
-            super(name, urlFile, instance);
+            super(file.getName(), instance);
+            this.file = file;
+            this.fileOnDisk = null; // We create this when we get our first write message
         }
         
+        public void write(StyxFileClient client, long offset, int count,
+            ByteBuffer data, boolean truncate, int tag)
+            throws StyxException
+        {
+            // We're not allowed to write to this file if the service is running
+            // or the input URL is set
+            if (instance.getStatus() == StatusCode.RUNNING)
+            {
+                throw new StyxException("Cannot write to an input file while " +
+                    "the service is running");
+            }
+            else if (this.urlFile.getURL() != null)
+            {
+                throw new StyxException("Cannot write to " + this.name +
+                    " because the service is reading from " + this.urlFile.getURL());
+            }
+            if (this.fileOnDisk == null)
+            {
+                try
+                {
+                    if(!this.file.createNewFile())
+                    {
+                        throw new StyxException("Could not create file " +
+                            this.file.getName());
+                    }
+                }
+                catch (IOException ioe)
+                {
+                    throw new StyxException("Error creating file " +
+                        this.file.getName() + ": " + ioe.getMessage());
+                }
+                this.fileOnDisk = new FileOnDisk(this.file);
+            }
+            this.fileOnDisk.write(client, offset, count, data, truncate, tag);
+            this.dataWritten = true;
+        }
         
+        /**
+         * @return true if data have been uploaded to this file and we have
+         * received an EOF message
+         */
+        public boolean dataUploadComplete()
+        {
+            if (this.fileOnDisk == null)
+            {
+                return false;
+            }
+            else
+            {
+                return this.fileOnDisk.receivedEOF();
+            }
+        }
+        
+        /**
+         * @return the length of the underlying file on disk
+         */
+        public ULong getLength()
+        {
+            if (this.fileOnDisk == null)
+            {
+                return ULong.ZERO;
+            }
+            else
+            {
+                return this.fileOnDisk.getLength();
+            }
+        }
     }
     
-    /*public static class FileFromParam extends SGSInputFile
+    /**
+     * @return the file that will be used to set the URL from which the file's
+     * data will be read
+     */
+    public InputURLFile getInputURLFile()
     {
-        private FileFromParam(String name) throws StyxException
+        return this.urlFile;
+    }
+    
+    /**
+     * Specialization of URLFile which checks to make sure the service is not
+     * running before allowing the URL to be set
+     */
+    public class InputURLFile extends URLFile
+    {
+        public InputURLFile(String name) throws StyxException
         {
             super(name);
         }
-    }*/
-    
-    
+
+        public synchronized void write(StyxFileClient client, long offset,
+            int count, ByteBuffer data, boolean truncate, int tag)
+            throws StyxException
+        {
+            if (instance.getStatus() == StatusCode.RUNNING)
+            {
+                throw new StyxException("Cannot set input URL while service is running");
+            }
+            else if (dataWritten)
+            {
+                throw new StyxException("Cannot set input URL after data have been" +
+                    " written to the file");
+            }
+            super.write(client, offset, count, data, truncate, tag);
+        }
+    }
     
 }
