ca4b4eb11eb738534cd1f82228314cd59156a51d
jonblower <jonblower@aeb1b72f-4910-0410-8ae4-b0e1c25358ee>
2006-05-22 16:18:53 +0000

    Removed references to $JSTYX_HOME and ensured that the DTD is picked up correctly
    git-svn-id: svn://svn.code.sf.net/p/jstyx/code/trunk@677 aeb1b72f-4910-0410-8ae4-b0e1c25358ee

--- core/bin/GridServices.bat
+++ core/bin/GridServices.bat
@@ -1,3 +1,9 @@
 @echo off
 
-call JStyxRun uk.ac.rdg.resc.jstyx.gridservice.server.StyxGridService "%JSTYX_HOME%\conf\SGSconfig.xml"
+REM Change to the directory containing the config XML and DTD.
+REM %~dp0 contains the directory from which this script runs.
+REM This ensures that the DTD is loaded correctly.
+
+cd %~dp0..\conf
+
+call JStyxRun uk.ac.rdg.resc.jstyx.gridservice.server.StyxGridService SGSconfig.xml
