b2fbdaed82254ddee029eae630aa144515f11e87
jonblower <jonblower@aeb1b72f-4910-0410-8ae4-b0e1c25358ee>
2006-05-25 08:10:24 +0000

    Attempt (unsuccessful) to change back to the current working directory when GridServices is finished
    git-svn-id: svn://svn.code.sf.net/p/jstyx/code/trunk@683 aeb1b72f-4910-0410-8ae4-b0e1c25358ee

--- core/bin/GridServices.bat
+++ core/bin/GridServices.bat
@@ -1,11 +1,15 @@
 @echo off
 
 REM Change to the drive and directory containing the config XML and DTD.
-REM %~d0 contains the drive (e.g. c:) on which this script lives.
 REM %~dp0 contains the drive and path of the directory on which this script lives.
 REM This ensures that the DTD is loaded correctly.
 
-%~d0
-cd %~dp0..\conf
+set cwd = %CD%
+cd /d %~dp0..\conf
 
 call JStyxRun uk.ac.rdg.resc.jstyx.gridservice.server.SGSServer SGSconfig.xml %*
+
+REM change back to the previous working directory
+REM this doesn't work if you quit the batch file with Control-C - never gets run
+echo %cwd%
+cd /d %cwd%
