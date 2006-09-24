6af5de64f88b75ecb5e0bae9de80a4552e83ef33
jonblower <jonblower@aeb1b72f-4910-0410-8ae4-b0e1c25358ee>
2006-09-24 20:27:58 +0000

    Fixed path to JStyxRun
    git-svn-id: svn://svn.code.sf.net/p/jstyx/code/trunk@722 aeb1b72f-4910-0410-8ae4-b0e1c25358ee

--- core/bin/GridServices.bat
+++ core/bin/GridServices.bat
@@ -7,7 +7,7 @@ REM This ensures that the DTD is loaded correctly.
 set cwd = %CD%
 cd /d %~dp0..\conf
 
-call JStyxRun uk.ac.rdg.resc.jstyx.gridservice.server.SGSServer SGSconfig.xml %*
+call ..\bin\JStyxRun uk.ac.rdg.resc.jstyx.gridservice.server.SGSServer SGSconfig.xml %*
 
 REM change back to the previous working directory
 REM this doesn't work if you quit the batch file with Control-C - never gets run
