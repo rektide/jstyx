8c382623fba1be10496f31d179217d9d06fc5d1e
jonblower <jonblower@aeb1b72f-4910-0410-8ae4-b0e1c25358ee>
2007-06-07 08:06:03 +0000

    Experimenting with Spring 2.0
    git-svn-id: svn://svn.code.sf.net/p/jstyx/code/trunk@779 aeb1b72f-4910-0410-8ae4-b0e1c25358ee

---
+++ G-Rex/code/src/java/uk/ac/rdg/resc/trex/servlet/GRexController.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright (c) 2007 The University of Reading
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the University of Reading, nor the names of the
+ *    authors or contributors may be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package uk.ac.rdg.resc.trex.servlet;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.AbstractController;
+import uk.ac.rdg.resc.trex.exceptions.GRexException;
+
+/**
+ * Entry point for the GRex web application.  Handles web requests.
+ *
+ * @author Jon Blower
+ * $Revision$
+ * $Date$
+ * $Log$
+ */
+public class GRexController extends AbstractController
+{
+
+    protected ModelAndView handleRequestInternal(HttpServletRequest request,
+        HttpServletResponse response) throws Exception
+    {
+        System.out.print("Supported methods:");
+        for (String method : this.getSupportedMethods())
+        {
+            System.out.print("   " + method);
+        }
+        System.out.println("");
+        if (true) throw new GRexException("This is an exception!");
+        // TODO: automatically append the file extension to the view name
+        if (request.getRequestURI().endsWith(".html"))
+        {
+            return new ModelAndView("hello_html"); // Maps to hello_html.jsp
+        }
+        else if (request.getRequestURI().endsWith(".xml"))
+        {
+            // Must be XML
+            return new ModelAndView("hello_xml"); // Maps to hello_xml.jsp
+        }
+        else
+        {
+            throw new Exception("Unknown file extension");
+        }
+    }
+    
+}
---
+++ G-Rex/code/web/WEB-INF/jsp/displayGRexException.jsp
@@ -0,0 +1,40 @@
+<%@page contentType="text/xml"%>
+<%@page pageEncoding="UTF-8"%>
+<%--
+The taglib directive below imports the JSTL library. If you uncomment it,
+you must also add the JSTL library to the project. The Add Library... action
+on Libraries node in Projects view can be used to add the JSTL 1.1 library.
+--%>
+<%--
+<%@taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%> 
+--%>
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+   "http://www.w3.org/TR/html4/loose.dtd">
+
+<%-- Used to display GRexExceptions --%>
+<html>
+    <head>
+        <title>ERROR!</title>
+    </head>
+    <body>
+
+    <h1>Error!</h1>
+    
+    <%-- The Spring framework injects the GRexException into this page and calls it "exception" --%>
+    <p>Message: ${exception.message}</p>
+    <p>Code: ${exception.errorCode}</p>
+    
+    <%--
+    This example uses JSTL, uncomment the taglib directive above.
+    To test, display the page like this: index.jsp?sayHello=true&name=Murphy
+    --%>
+    <%--
+    <c:if test="${param.sayHello}">
+        <!-- Let's welcome the user ${param.name} -->
+        Hello ${param.name}!
+    </c:if>
+    --%>
+    
+    </body>
+</html>
